38308e614b8362dfd31c55963c64a966f8d05624
YosukeOba <52928718+YosukeOba@users.noreply.github.com>
2022-09-13 18:04:26 +0900

    できた

--- ABC268/C.swift
+++ ABC268/C.swift
@@ -4,40 +4,30 @@ var dish = readLine()!.split(separator: " ").map { Int(String($0))! }
 var max:Int = 0
 var count:Int = 0
 
-var lu:[Int] = []
-
-for i in 0..<num {
-    let result = dish.firstIndex(of: i)
-    lu += [result!]
-  	let temp:Int = dish[0]
-  	dish.removeFirst()
-  	dish += [temp]
-}
-
 var intArray = [Int](repeating: 0, count: num)
 
 for i in 0..<num {
-    intArray[lu[i]] += 1
-    if(lu[i]-1 < 0){
-        intArray[num-1] += 1
+    if(dish[i]-i+1 < 0){
+        intArray[num+(dish[i]-i+1)] += 1
+        intArray[num+(dish[i]-i)] += 1
+        intArray[num+(dish[i]-i-1)] += 1
+    } else if(dish[i]-i < 0){
+        intArray[dish[i]-i+1] += 1
+        intArray[num+(dish[i]-i)] += 1
+        intArray[num+(dish[i]-i-1)] += 1
+    } else if(dish[i]-i-1 < 0){
+        intArray[dish[i]-i+1] += 1
+        intArray[dish[i]-i] += 1
+        intArray[num+(dish[i]-i-1)] += 1
     } else {
-        intArray[lu[i]-1] += 1
-    }
-    if(lu[i]+1 >= num){
-        intArray[0] += 1
-    } else {
-        intArray[lu[i]+1] += 1
+        intArray[dish[i]-i+1] += 1
+        intArray[dish[i]-i] += 1
+        intArray[dish[i]-i-1] += 1
     }
 }
 
-for i in 0..<num {
-    if(max < intArray[i]){
-        max = intArray[i]
-    }
+if(intArray.max()! > num){
+  print(num)
+} else {
+    print(intArray.max()!)
 }
-
-if(max > num){
-  max = num
-}
-
-print(max)
