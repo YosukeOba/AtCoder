e77200ea9e7c7f31f4bae7531568e219e0e9dc6c
YosukeOba <52928718+YosukeOba@users.noreply.github.com>
2022-09-13 13:01:11 +0900

    だいぶ修正したけどTLE

--- ABC268/C.swift
+++ ABC268/C.swift
@@ -4,27 +4,40 @@ var dish = readLine()!.split(separator: " ").map { Int(String($0))! }
 var max:Int = 0
 var count:Int = 0
 
+var lu:[Int] = []
+
 for i in 0..<num {
-    count = 0
-    for j in 0..<num {
-        var minus:Int = j-1
-        if(minus < 0){
-            minus = num - 1
-        }
-        var plus:Int = j+1
-        if(plus >= num){
-            plus = 0
-        }
-        if(j == dish[minus] || j == dish[j] || j == dish[plus]){
-            count+=1
-        }
+    let result = dish.firstIndex(of: i)
+    lu += [result!]
+  	let temp:Int = dish[0]
+  	dish.removeFirst()
+  	dish += [temp]
+}
+
+var intArray = [Int](repeating: 0, count: num)
+
+for i in 0..<num {
+    intArray[lu[i]] += 1
+    if(lu[i]-1 < 0){
+        intArray[num-1] += 1
+    } else {
+        intArray[lu[i]-1] += 1
     }
-    if(max < count){
-        max = count
+    if(lu[i]+1 >= num){
+        intArray[0] += 1
+    } else {
+        intArray[lu[i]+1] += 1
     }
-    var temp:Int = dish[0]
-    dish.removeFirst()
-    dish += [temp]
+}
+
+for i in 0..<num {
+    if(max < intArray[i]){
+        max = intArray[i]
+    }
+}
+
+if(max > num){
+  max = num
 }
 
 print(max)
