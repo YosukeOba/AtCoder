a4cbfc0b92fdfb7c6422a59302c66e067fa707c0
YosukeOba <52928718+YosukeOba@users.noreply.github.com>
2022-09-28 16:54:54 +0900

    通った...（涙）

--- ABC270/C.swift
+++ ABC270/C.swift
@@ -17,33 +17,41 @@ while true {
 }
 
 var queue:[Int] = []
+var answerQueue:[Int] = []
 
-var check:Int = 0 
+var check:Int = 0
+
+var checks:[Int] = [Int](repeating: 0, count: num.count+1)
+checks[line[1]] = 1
 
 queue += [line[1]]
 
 saiki(lastnum: queue[queue.count-1])
 
-var answer:String = String(queue[0])
+var answer:String = String(answerQueue[0])
 
-for i in 1..<queue.count{
-  answer += " " + String(queue[i])
+for i in 1..<answerQueue.count{
+  answer += " " + String(answerQueue[i])
 }
 
 print(answer)
 
 func saiki(lastnum: Int){
   for i in 0..<num[lastnum].count{
-    if(check == 0 && queue.firstIndex(of: num[lastnum][i]) == nil){
+    if(check == 0 && checks[num[lastnum][i]] == 0){
       queue += [num[lastnum][i]]
+      checks[num[lastnum][i]] = 1
       if(num[lastnum][i] == line[2]){
         check = 1
+        answerQueue = queue
         return
       } else {
         if(num[num[lastnum][i]].count == 1){
+          checks[num[lastnum][i]] = 0
           queue.removeLast()
         } else {
           saiki(lastnum: num[lastnum][i])
+          queue.removeLast()
         }
       }
     }
